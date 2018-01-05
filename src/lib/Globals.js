5ef990cdb1366306c15f1a5b330cce12038b61a0
Yu-Ann Chen <red8012@gmail.com>
2018-01-05 23:18:34 +0800

    WIP: Migrated CodeMirror

--- src/lib/Globals.js
+++ src/lib/Globals.js
@@ -1,3 +1,5 @@
-const g = {}
+const g = {
+    CODEMIRROR_ROOT: 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.33.0/'
+}
 
 export default g
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import App from './App.html'
 
 const app = new App({
     target: document.body,
-    data: {
-        name: 'world'
-    }
 })
 
 export default app
