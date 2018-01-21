dff49ba73fb4e1b7eb610bd321898c19f76d3cec
Yu-Ann Chen <red8012@gmail.com>
2018-01-21 08:50:07 +0800

    CHECKPOINT: before implementing new file tree

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+//import postcss from 'rollup-plugin-postcss'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -27,7 +28,10 @@ export default {
         commonjs({
             namedExports: {}
         }),
-        
+        //        postcss({
+        //            plugins: [],
+        //            minimize: true
+        //        }),
         production && babel()
     ]
 }
