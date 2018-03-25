283656fcce4c8aeadd069ea932bee2ab00c3a9e6
Yu-Ann Chen <red8012@gmail.com>
2018-03-25 22:51:24 +0800

    NEW: schedule panel loading by onIdle function

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -10,6 +10,24 @@ function binarySearch(array, target) {
     return hi
 }
 
+function onIdle(callback, timeout = 7000, delay = 1) {
+    setTimeout(() => { // let other things run first
+        if (window.requestIdleCallback) {
+            window.requestIdleCallback(() => {
+                requestAnimationFrame(callback)
+            }, {
+                timeout
+            })
+        } else {
+            requestAnimationFrame(() => {
+                console.warn('requestIdleCallback not available')
+                requestAnimationFrame(callback)
+            })
+        }
+    }, delay)
+}
+
 export {
-    binarySearch
+    binarySearch,
+    onIdle,
 }
