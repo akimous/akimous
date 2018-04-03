de3bc4122f2cfb49ebba684bf35fd9f310e38342
Yu-Ann Chen <red8012@gmail.com>
2018-04-03 23:15:51 +0800

    NEW: added tooltip for toolbar icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import App from './App.html'
 import 'normalize.css/normalize.css'
 import 'codemirror/lib/codemirror.css'
 import 'xterm/dist/xterm.css'
+import 'primer-tooltips/build/build.css'
 
 const app = new App({
     target: document.body,
