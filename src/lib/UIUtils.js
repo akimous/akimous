93653e19dd66646248f3517add1a2a19a51c9e0b
Yu-Ann Chen <red8012@gmail.com>
2018-06-14 23:16:29 +0800

    NEW: initial keyboard control hint outline

---
+++ src/lib/UIUtils.js
@@ -0,0 +1,41 @@
+function dragElement(element) {
+    let xDiff = 0,
+        yDiff = 0,
+        xStart = 0,
+        yStart = 0
+
+    element.onmousedown = startDragging
+
+    function startDragging(e) {
+        xStart = e.clientX
+        yStart = e.clientY
+        document.onmouseup = endDragging
+        document.onmousemove = onDragging
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    function onDragging(e) {
+        requestAnimationFrame(() => {
+            xDiff = xStart - e.clientX
+            yDiff = yStart - e.clientY
+            xStart = e.clientX
+            yStart = e.clientY
+            element.style.top = `${(element.offsetTop - yDiff)}px`
+            element.style.left = `${(element.offsetLeft - xDiff)}px`
+            e.preventDefault()
+            e.stopPropagation()
+        })
+    }
+
+    function endDragging(e) {
+        document.onmouseup = null
+        document.onmousemove = null
+        e.preventDefault()
+        e.stopPropagation()
+    }
+}
+
+export {
+    dragElement
+}
