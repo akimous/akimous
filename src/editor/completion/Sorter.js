508718cc936f91753f2e3020b44cdb606ef5e9e6
Yu-Ann Chen <red8012@gmail.com>
2018-10-05 18:45:41 +0800

    FIX: sorter weightning

--- src/editor/completion/Sorter.js
+++ src/editor/completion/Sorter.js
@@ -97,7 +97,7 @@ class Sorter {
         const MATCH_LOWER = 5,
             MATCH_UPPER = 10,
             SKIP = -1,
-            UNMATCHED = -50,
+            UNMATCHED = -100,
             MIN = -9999,
             MATCH_FIRST_CHAR_CASE_SENSITIVE = 100
         this.target = target
