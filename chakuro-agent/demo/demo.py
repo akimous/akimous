fb953301268a17dd5b85a1803b5ea3372cb393e9
Yu-Ann Chen <red8012@gmail.com>
2018-12-09 13:02:01 +0800

    NEW: initial outline implementation

--- chakuro-agent/demo/demo.py
+++ chakuro-agent/demo/demo.py
@@ -85,6 +85,9 @@ def pep8_indentation():
     def long_function_name(var_one, var_two, var_three, var_four):
         print(var_one)
 
+        def deep_function():
+            pass
+
     # Hanging indents should add a level.
     foo = long_function_name(var_one, var_two, var_three, var_four)
 
