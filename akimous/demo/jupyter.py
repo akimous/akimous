ce76f3942ac355a9738903fc35500f28e04f097f
Yu-Ann Chen <red8012@gmail.com>
2019-07-21 21:06:46 +0800

    FIX: undefined cell of matplotlib plot; katex failed to display

--- akimous/demo/jupyter.py
+++ akimous/demo/jupyter.py
@@ -4,13 +4,7 @@ from matplotlib import pyplot as plt
 from sympy import *
 
 plt.style.use('dark_background')
-#%matplotlib inline
-'yoyoyo'
-print('pppp')
-
 plt.plot([1, 2], [3, 4])
-
-time.sleep(3)
 
 init_printing(use_latex=True)
 a, b, c, x = symbols('a b c x')
