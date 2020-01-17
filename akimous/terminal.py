66666d801a738d98d667755c232dd5acf115f770
Yu-Ann Chen <red8012@gmail.com>
2020-01-17 13:08:58 +0800

    FIX: allow console to work in non-UNIX OSes

--- akimous/terminal.py
+++ akimous/terminal.py
@@ -1,5 +1,6 @@
 import asyncio
 import shlex
+from asyncio.subprocess import PIPE, STDOUT
 from functools import partial
 from pathlib import Path
 
@@ -8,11 +9,13 @@ from logzero import logger
 from .utils import nop
 from .websocket import register_handler
 
+IS_PTY = False
 try:
     from ptyprocess import PtyProcessUnicode
+    IS_PTY = True
 except ModuleNotFoundError:
-    logger.warning('ptyprocess does not support Windows, '
-                   'terminal function will be disabled')
+    logger.warning('ptyprocess is not available, '
+                   'terminal function will behave differently')
 
 handles = partial(register_handler, 'terminal')
 
@@ -29,6 +32,36 @@ def reader(pty, send, context):
                 send('Stdout',
                      f'(Process finished with exit code {exit_code})')
             return
+        except asyncio.CancelledError:
+            return
+
+
+async def async_reader(pty, send):
+    logger.info('reader started')
+    source = pty.stdout
+    while True:
+        try:
+            message = await source.read(65536)
+            if message:
+                await send('Stdout', message)
+            else:
+                await asyncio.sleep(.01)
+            if source.at_eof():
+                return
+        except asyncio.CancelledError:
+            return
+        except Exception as e:
+            logger.exception(e)
+
+
+async def wait_until_finish(pty, send):
+    try:
+        await pty.wait()
+        await asyncio.sleep(.01)
+        await send('Stdout',
+                   f'(Process finished with exit code {pty.returncode})')
+    except Exception:
+        logger.exception(e)
 
 
 @handles('_connected')
@@ -64,22 +97,43 @@ async def run_in_terminal(msg, send, context):
     if not cwd:
         cwd = str(root)
     cwd = shlex.quote(cwd)
-    pty = PtyProcessUnicode.spawn(command,
-                                  cwd=cwd,
-                                  dimensions=(msg['rows'], msg['cols']))
-    await send('Started', None)
 
-    context.pty = pty
-    loop = asyncio.get_event_loop()
-    context.reader = asyncio.ensure_future(
-        loop.run_in_executor(
-            None, partial(reader, pty, context.main_thread_send, context)))
+    if IS_PTY:
+        pty = PtyProcessUnicode.spawn(command,
+                                      cwd=cwd,
+                                      dimensions=(msg['rows'], msg['cols']))
+        await send('Started', None)
+        context.pty = pty
+        loop = asyncio.get_event_loop()
+        context.reader = asyncio.ensure_future(
+            loop.run_in_executor(
+                None, partial(reader, pty, context.main_thread_send, context)))
+    else:
+        pty = await asyncio.create_subprocess_shell(shlex.join(command),
+                                                    stdin=PIPE,
+                                                    stdout=PIPE,
+                                                    stderr=STDOUT,
+                                                    cwd=cwd,
+                                                    bufsize=0)
+        await send('Started', None)
+        context.pty = pty
+        context.reader = asyncio.create_task(async_reader(pty, send))
+        context.reader = asyncio.create_task(wait_until_finish(pty, send))
 
 
 @handles('Stdin')
-async def stdin(msg, send, context):
-    if context.pty:
-        context.pty.write(msg)
+async def stdin(msg: str, send, context):
+    pty = context.pty
+    logger.warning('%r, %r', pty, IS_PTY)
+    if pty:
+        if IS_PTY:
+            pty.write(msg)
+        else:
+            if msg == '\r':
+                msg = '\n'
+            pty.stdin.write(msg.encode('utf-8'))
+            await pty.stdin.drain()
+            await send('Stdout', msg)
 
 
 @handles('Stop')
@@ -88,5 +142,8 @@ async def stop(msg, send, context):
     if pty:
         if context.reader:
             context.reader.cancel()
-        pty.terminate(force=True)
-    await send('Stdout', '\n(Process terminated.)')
+        if IS_PTY:
+            pty.terminate(force=True)
+        elif pty.returncode is None:
+            pty.kill()
+        await send('Stdout', '\n(Process terminated.)')
