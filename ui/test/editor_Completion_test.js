61323307f7ba43826f3827482704cad425e64173
Yu-Ann Chen <red8012@gmail.com>
2020-09-27 21:35:43 +0800

    TEST: try to fix ui test timeout

--- ui/test/editor_Completion_test.js
+++ ui/test/editor_Completion_test.js
@@ -19,6 +19,7 @@ Scenario('Without realtime formatting', async (I) => {
     I.waitForCompletionOrContinueIn(5)
     I.typeAndCompare(['ph'])
     I.typeAndCompare(['.'], ['from sphinx.'])
+    I.waitForCompletionOrContinueIn(5)
     I.typeAndCompare(['io im '], ['from sphinx.io import '])
     I.typeAndCompare(['rea', 'Enter'], ['from sphinx.io import read_doc'])
     I.clear()
